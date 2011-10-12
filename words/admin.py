6a7756430c31868a86d82d751f4d17b0b0b5d036
matt <m@ttforbes.com>
2011-10-11 19:41:57 -0700

    removed html

--- words/admin.py
+++ words/admin.py
@@ -16,11 +16,10 @@ class PlaylistAdmin(admin.ModelAdmin):
         if not kwargs.get('change', True):
             from django.core.mail import send_mail
             # new playlist, mass email subscribers
-            msg = '<h2>New Playlist, %s, at wordplaylist.com</h2>' % obj.name
-            msg += '<div>Words included in %s:<ul>' % obj.name
+            msg = 'New Playlist, %s, at wordplaylist.com\n\n' % obj.name
+            msg += 'Words included in %s:\n' % obj.name
             for word in obj.word_set.all():
-                msg += "<li>%s</li>" % word.word
-                msg += "</ul></div>"
+                msg += "%s\n" % word.word
             #send the email
             send_mail('subject', msg, 'info@wordplaylist.com', [e.address for e in Email.objects.all()], fail_silently=False)
             
